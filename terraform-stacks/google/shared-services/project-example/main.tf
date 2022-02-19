1a27a1bafaf05a335b5c9d8c4fdb93efffb69697
Jesse Cota <34288347+jaylabs@users.noreply.github.com>
2022-02-19 16:45:17 -0300

    Update main.tf

--- terraform-stacks/google/shared-services/project-example/main.tf
+++ terraform-stacks/google/shared-services/project-example/main.tf
@@ -1,5 +1,5 @@
 #resource "google_storage_bucket" "bucket" {
-  name          = "jaylabs-bucket-example1"
+  name          = "jaylabs-bucket-example"
   project       = "core-nomad-245305"
   location      = "EU"
   force_destroy = true
