d28bb0aaaec73880d7572bfa4518ed0b905486e6
Jesse Cota <jc@maxxer.com>
2022-02-19 16:38:38 -0300

    Update workflow file

--- terraform-stacks/google/shared-services/project-example/main.tf
+++ terraform-stacks/google/shared-services/project-example/main.tf
@@ -1,1 +1,19 @@
-#
+#resource "google_storage_bucket" "bucket" {
+  name          = "jaylabs-bucket-exampple"
+  project       = "core-nomad-245305"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
